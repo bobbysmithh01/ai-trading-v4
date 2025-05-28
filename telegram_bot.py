f1f02770775d4bca83812233b8400b89f6927d28
bobbysmithh01 <bobby.smith06@hotmail.com>
2025-05-28 11:21:19 +0100

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,7 +2,7 @@
 import requests
 
 def send_telegram_alert(message):
-    token = "YOUR_BOT_TOKEN"
+    token = "7710715037:AAGNf0JKGeQi37HzySiSOM8tsZr_unnXoXg"
     chat_id = "@bobbysmithh01"
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     payload = {"chat_id": chat_id, "text": message}
