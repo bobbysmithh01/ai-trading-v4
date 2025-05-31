b74607b1f3880eadf59e5d29cf18199733d4e40d
bobbysmithh01 <bobby.smith06@hotmail.com>
2025-05-31 20:06:40 +0100

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,15 +1,13 @@
+
 import requests
 
 TELEGRAM_BOT_TOKEN = "7710715037:AAGNf0JKGeQi37HzySiSOM8tsZr_unnXoXg"
 TELEGRAM_CHAT_ID = "bobbysmithh01"
 
 def send_telegram_alert(message):
-    if TELEGRAM_BOT_TOKEN == "7710715037:AAGNf0JKGeQi37HzySiSOM8tsZr_unnXoXg":
-        print("[TELEGRAM MOCK] " + message)
-        return
     try:
-        url = f"https://api.telegram.org/bot7710715037:{AAGNf0JKGeQi37HzySiSOM8tsZr_unnXoXg}/sendMessage"
+        url = f"https://api.telegram.org/bot{7710715037:AAGNf0JKGeQi37HzySiSOM8tsZr_unnXoXg}/sendMessage"
         data = {"chat_id": bobbysmithh01, "text": message}
         requests.post(url, json=data)
     except Exception as e:
-        print(f"Telegram error: {e}")
+        print("Telegram error:", e)
