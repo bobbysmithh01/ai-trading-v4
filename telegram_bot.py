a7f799eb99fa771fe58a10d212c0bf6e5a41c1b3
bobbysmithh01 <bobby.smith06@hotmail.com>
2025-05-31 20:19:50 +0100

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,7 +7,7 @@ TELEGRAM_CHAT_ID = "bobbysmithh01"
 def send_telegram_alert(message):
     try:
         url = f"https://api.telegram.org/bot{7710715037:AAGNf0JKGeQi37HzySiSOM8tsZr_unnXoXg}/sendMessage"
-        data = {"chat_id": bobbysmithh01, "text": message}
+        data = {"chat_id": 6835207640, "text": message}
         requests.post(url, json=data)
     except Exception as e:
         print("Telegram error:", e)
